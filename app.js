0c6cc2749e9a6274a95957b7a92d177c58b13cb0
zachpuck <zachpuckett@gmail.com>
2017-09-25 21:48:22 -0700

    add individual recipe route

--- app.js
+++ app.js
@@ -4,46 +4,13 @@ const recipe = {};
 const app = express();
 const port = process.env.PORT || 5000;
 
-const recipeRouter = express.Router();
+const recipeRouter = require('./src/routes/recipeRoutes');
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-let recipes = [
-    {
-        title: 'chicken pot pie',
-        Ingredients: 'pie crust, peas, carrots, chicken',
-        time: '1 hr',
-        haveCooked: false
-    },
-    {
-        title: 'ramen noodles',
-        Ingredients: 'noodles, shrimp, dashi, mushrooms',
-        time: '30 mins',
-        haveCooked: true
-    }
-];
 
-recipeRouter.route('/')
-    .get(function(req, res) {
-        res.render('recipes', {
-            title: 'recipe list',
-            nav: [{
-                    Link: '/Recipes',
-                    Text: 'Recipes'
-                }, {
-                    Link: '/Ingredients',
-                    Text: 'Ingredients'
-                }],
-            recipes: recipes
-        });
-    });
-
-recipeRouter.route('/quick')
-    .get(function(req, res) {
-        res.send('hello quick recipes');
-    });
 
 app.use('/Recipes', recipeRouter);
 
